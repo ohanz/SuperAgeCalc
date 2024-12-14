155198566c5c9d3965f861a5a4fa4673f2ebed93
Ohanz <chiaguguadaniel@gmail.com>
2024-12-14 05:32:43 +0100

    first commit

---
+++ myScript.js
@@ -0,0 +1,18 @@
+let currentDate= document.getElementById("currentDate");
+let dateOfBirth = document.querySelector("#DOB");
+const CalcAge= document.getElementById("CalcAge");
+const displayAge= document.getElementById("displayAge");
+const Age= document.getElementById("Age");
+var today = new Date();
+currentDate.innerText=`Today's Date is : ${today.toLocaleDateString('en-US')}`;
+
+CalcAge.addEventListener("click",()=>{
+    var birthDate = new Date(dateOfBirth.value);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age = age - 1;
+    }
+displayAge.style.visibility="visible";
+Age.innerText=`You are ${age} years old.`
+});
